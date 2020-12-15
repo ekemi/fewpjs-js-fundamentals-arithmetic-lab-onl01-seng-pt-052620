3570b2709b9b135bcaabc744d2e1199ceb553139
edmundo papi <edmundopierre@yahoo.com>
2020-12-15 13:17:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 
 // Write your code here
+let num1,num2;
+ function multiply(num1,num2){
+  if (Number.isInteger(num1)&& Number.isInteger(num2)){
+    let result = num1 * num2;
+    return (result).toString;
+  }
+}
